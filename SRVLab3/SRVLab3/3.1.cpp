546cb7d86bb34246ce1b017d650b03400f14660c
Lis4Game <rusakov_vladislav_2005@mail.ru>
2025-12-06 15:15:59 +0200

    Fix1

--- SRVLab3/SRVLab3/3.1.cpp
+++ SRVLab3/SRVLab3/3.1.cpp
@@ -5,36 +5,47 @@
 int coins = 101;
 int Bob_coins = 0;
 int Tom_coins = 0;
+int Dead_coins = 0;
 std::mutex mtx;
 
 void coin_sharing(std::string name, int& thief_coins, int& companion_coins) {
     while (true) {
         std::lock_guard<std::mutex> lock(mtx);
-        int coins_to_divide = (coins % 2 == 1) ? coins - 1 : coins;
-        if (coins_to_divide <= 0) {
+
+        if (coins == 1 && Bob_coins == Tom_coins) {
             break;
         }
+
+        if (coins <= 0) {
+            break;
+        }
+
         if (thief_coins <= companion_coins) {
             coins--;
             thief_coins++;
 
-            std::cout << name << " : " << thief_coins << " " << companion_coins << " |Left: " << coins << std::endl;
+            std::cout << name << " :" << thief_coins << " " << companion_coins
+                << " Осталось: " << coins << std::endl;
         }
-
     }
 }
+
 int main() {
+    system("chcp 1251>nul");
     std::thread bob_thread(coin_sharing, "Bob", std::ref(Bob_coins), std::ref(Tom_coins));
     std::thread tom_thread(coin_sharing, "Tom", std::ref(Tom_coins), std::ref(Bob_coins));
 
     bob_thread.join();
     tom_thread.join();
 
-    std::cout << "\nSummary:\n";
-    std::cout << "Bob: " << Bob_coins << "\n";
-    std::cout << "Tom: " << Tom_coins << "\n";
-    std::cout << "Dead: " << coins << "\n";
-    std::cout << "All: " << Bob_coins + Tom_coins + coins << "\n";
+    if (coins > 0 && Bob_coins == Tom_coins) {
+        Dead_coins = coins;
+        coins = 0;
+    }
+
+    std::cout << "Итого: Bob=" << Bob_coins << ", Tom=" << Tom_coins
+        << ", Покойник=" << Dead_coins << ", Осталось=" << coins << std::endl;
+    std::cout << "Сумма: " << (Bob_coins + Tom_coins + Dead_coins + coins) << std::endl;
+
     return 0;
 }
-
