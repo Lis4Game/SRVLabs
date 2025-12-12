3e4a9706a0d5b07800214a0d04f8e177913f0ee2
Lis4Game <rusakov_vladislav_2005@mail.ru>
2025-12-12 14:55:15 +0200

    Lab5

---
+++ SRVLab5/SRVLab5/5.1.cpp
@@ -0,0 +1,146 @@
+#include <iostream>
+#include <thread>
+#include <atomic>
+#include <chrono>
+#include <vector>
+#include <cstdlib>
+
+class PsevdoMutex {
+private:
+    std::atomic<bool> locked{ false };
+
+public:
+    void lock() {
+        while (locked.exchange(true)) {
+        }
+    }
+
+    void unlock() {
+        locked = false;
+    }
+};
+
+const int EXPLOSION_LIMIT = 10000;
+const int INITIAL_DISH = 3000;
+const int NUM_FAT_MEN = 3;
+const int SIMULATION_TIME_SEC = 5;
+
+PsevdoMutex mtx;
+
+struct Result {
+    std::string outcome;
+    std::vector<int> eaten;
+    std::vector<int> dishes;
+};
+
+Result run_simulation(int gluttony, int efficiency) {
+    std::vector<int> dishes(NUM_FAT_MEN, INITIAL_DISH);
+    std::vector<int> eaten(NUM_FAT_MEN, 0);
+    bool running = true;
+    std::string outcome = "Кук уволился сам";
+
+    auto cook = std::thread([&]() {
+        while (running) {
+            std::this_thread::sleep_for(std::chrono::milliseconds(10));
+            mtx.lock();
+            if (!running) {
+                mtx.unlock();
+                break;
+            }
+            for (int i = 0; i < NUM_FAT_MEN; ++i) {
+                dishes[i] += efficiency;
+            }
+            mtx.unlock();
+        }
+        });
+
+    std::vector<std::thread> eaters;
+    for (int id = 0; id < NUM_FAT_MEN; ++id) {
+        eaters.emplace_back([&, id]() {
+            while (running) {
+                std::this_thread::sleep_for(std::chrono::milliseconds(10));
+                mtx.lock();
+                if (!running) {
+                    mtx.unlock();
+                    break;
+                }
+
+                if (eaten[id] >= EXPLOSION_LIMIT) {
+                    mtx.unlock();
+                    continue;
+                }
+
+                int room = EXPLOSION_LIMIT - eaten[id];
+                int take = std::min({ dishes[id], gluttony, room });
+
+                if (take > 0) {
+                    dishes[id] -= take;
+                    eaten[id] += take;
+
+                    if (dishes[id] <= 0) {
+                        outcome = "Кука уволили";
+                        running = false;
+                    }
+                }
+
+                bool all_exploded = true;
+                for (int i = 0; i < NUM_FAT_MEN; ++i) {
+                    if (eaten[i] < EXPLOSION_LIMIT) {
+                        all_exploded = false;
+                        break;
+                    }
+                }
+                if (all_exploded) {
+                    outcome = "Кук не получил зарплату";
+                    running = false;
+                }
+                mtx.unlock();
+            }
+            });
+    }
+
+    std::this_thread::sleep_for(std::chrono::seconds(SIMULATION_TIME_SEC));
+    {
+        mtx.lock();
+        running = false;
+        mtx.unlock();
+    }
+
+    cook.join();
+    for (auto& t : eaters) t.join();
+
+    return { outcome, eaten, dishes };
+}
+
+int main() {
+    system("chcp 1251>nul");
+
+    struct Test {
+        int gluttony;
+        int efficiency;
+    };
+
+    std::vector<Test> tests = {
+        {80, 5},    //Увольнение
+        {100, 1000}, //Взрыв
+        {1, 2}     //рейджквит
+    };
+
+    for (size_t i = 0; i < tests.size(); ++i) {
+        std::cout << "\nВариант " << (i + 1) << ":\n";
+        std::cout << "GLUTTONY = " << tests[i].gluttony
+            << ", EFFICIENCY = " << tests[i].efficiency << "\n";
+
+        Result res = run_simulation(tests[i].gluttony, tests[i].efficiency);
+
+        std::cout << "Итог: " << res.outcome << "\n";
+        std::cout << "Сводка:\n";
+        for (int j = 0; j < NUM_FAT_MEN; ++j) {
+            std::cout << "Толстяк #" << (j + 1)
+                << ": съел " << res.eaten[j]
+                << ", на тарелке " << res.dishes[j] << "\n";
+        }
+    }
+
+    return 0;
+}
